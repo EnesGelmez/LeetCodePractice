6fcdb0d0e1afb0d5479021cd381aecaa6d2c01a8
enesgelmez <enes.gelmez@hotmail.com>
2022-01-17 16:41:55 +0300

    Add project files.

---
+++ IsPalindrome(Easy)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IsPalindrome_Easy_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(IsPalindrome());
+        }
+
+        public static bool IsPalindrome()
+        {
+            int value = -121;
+            int result = 0;
+            if (value < 0 || ((value % 10 == 0) && value != 0)) return false;
+
+            while (value > result)
+            {
+                int curr = value % 10;
+                value /= 10;
+                result = result * 10 + curr;
+                Console.WriteLine(result);
+            }
+            return value == result || value == result / 10;
+        }
+
+    }
+}
---
+++ LeetCodePractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
